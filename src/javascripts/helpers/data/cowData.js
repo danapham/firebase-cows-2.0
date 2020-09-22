299185d1fa16f5acd5921336b1029e65c3e30f54
Dana Pham <danapham12@gmail.com>
2020-09-21 21:19:40 -0500

    added cards to farmers and cows view

--- src/javascripts/helpers/data/cowData.js
+++ src/javascripts/helpers/data/cowData.js
@@ -4,6 +4,17 @@ import apiKeys from '../apiKeys.json';
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
 // STUDENTS: Refactor this to use new Promise syntax
-const getCows = () => axios.get(`${baseUrl}/cows.json`);
+const getAllCows = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/cows.json`).then((response) => {
+    const doseCows = response.data;
+    const cows = [];
+    if (doseCows) {
+      Object.keys(doseCows).forEach((cowId) => {
+        cows.push(doseCows[cowId]);
+      });
+    }
+    resolve(cows);
+  }).catch((error) => reject(error));
+});
 
-export default { getCows };
+export default { getAllCows };
