540b161e66861d470f48822680e366f58fe156a1
Dana Pham <danapham12@gmail.com>
2020-09-22 21:30:48 -0500

    completed form

--- src/javascripts/helpers/data/cowData.js
+++ src/javascripts/helpers/data/cowData.js
@@ -17,10 +17,31 @@ const getAllCows = () => new Promise((resolve, reject) => {
   }).catch((error) => reject(error));
 });
 const deleteCow = (firebaseKey) => axios.delete(`${baseUrl}/cows/${firebaseKey}.json`);
+
 const addCow = (data) => axios.post(`${baseUrl}/cows.json`, data)
   .then((response) => {
     const update = { firebaseKey: response.data.name };
     axios.patch(`${baseUrl}/cows/${response.data.name}.json`, update);
   }).catch((error) => console.warn(error));
 
-export default { getAllCows, deleteCow, addCow };
+const getFarmerCows = (farmerUid) => new Promise((resolve) => {
+  axios.get(`${baseUrl}/cows.json?orderBy="farmerUid"&equalTo"${farmerUid}"`)
+    .then((response) => {
+      const farmerCows = response.data;
+      const cows = [];
+      if (farmerCows) {
+        Object.keys(farmerCows).forEach((cowId) => {
+          cows.push(farmerCows[cowId]);
+        });
+      }
+
+      resolve(cows);
+    }).catch((error) => console.warn(error));
+});
+
+export default {
+  getAllCows,
+  deleteCow,
+  addCow,
+  getFarmerCows
+};
