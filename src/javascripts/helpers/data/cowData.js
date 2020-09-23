1dd67df45b9415057e1e92c4a695b473983e176e
Dana Pham <danapham12@gmail.com>
2020-09-22 20:49:01 -0500

    cow form actually creates cow object

--- src/javascripts/helpers/data/cowData.js
+++ src/javascripts/helpers/data/cowData.js
@@ -16,7 +16,11 @@ const getAllCows = () => new Promise((resolve, reject) => {
     resolve(cows);
   }).catch((error) => reject(error));
 });
-
 const deleteCow = (firebaseKey) => axios.delete(`${baseUrl}/cows/${firebaseKey}.json`);
+const addCow = (data) => axios.post(`${baseUrl}/cows.json`, data)
+  .then((response) => {
+    const update = { firebaseKey: response.data.name };
+    axios.patch(`${baseUrl}/cows/${response.data.name}.json`, update);
+  }).catch((error) => console.warn(error));
 
-export default { getAllCows, deleteCow };
+export default { getAllCows, deleteCow, addCow };
